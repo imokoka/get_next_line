5df68aea8e685de17e8ab007a4052ed66efd4ae0
Itumeleng MOKOKA <imokoka@c4r22s4.wethinkcode.co.za>
2019-07-05 21:44:26 +0200

    adjustments will be made

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: imokoka <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/03 09:19:26 by imokoka           #+#    #+#             */
-/*   Updated: 2019/07/03 09:23:53 by imokoka          ###   ########.fr       */
+/*   Updated: 2019/07/05 21:42:48 by imokoka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 # define BUFF_SIZE 8
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
 # include "libft/libft.h"
 
 int		get_next_line(const int fd, char **line);
